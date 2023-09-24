42703e379e6bb2fea8e76afeaff5a33fbb4b58cd
rng <ray@perfectcast.com>
2023-09-24 17:16:39 +0100

    bug fix

--- main.go
+++ main.go
@@ -163,7 +163,7 @@ func buildOffsets(tuning string) ([]int, []string) {
 func main() {
 	var strIntervals  = flag.String("i", "2212221", "the intervals of the scale in semitones")
 	var mode = flag.Int("m", 1, "mode of the specified scale")
-	var active = flag.String("a", "1111111", "which notes of the scale are active")
+	var active = flag.String("a", "111111111111", "which notes of the scale are active. One bit per degree, extra bits ignored")
 	var tonic = flag.Int("s", 8, "fret of the tonic note on the lowest string")
 	var tuning = flag.String("t", "enbngndnanen", "the tuning of the instrument in descending order of pitch (works for any number of strings)")
 	var intervals []int
